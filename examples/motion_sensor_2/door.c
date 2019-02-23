aa460b5cfbdc3727059c78df5d8d27a630c11bf1
Aleksander Khmelenko <aleksanderkhmelenko@iMac-Aleksander.local>
2019-02-23 11:16:46 +0400

    3

--- examples/motion_sensor_2/door.c
+++ examples/motion_sensor_2/door.c
@@ -14,7 +14,7 @@
 
 #define MOTION_SENSOR_GPIO 14
 
-#define DEBOUNCE_TIME 500 / portTICK_PERIOD_MS
+#define DEBOUNCE_TIME 5000 / portTICK_PERIOD_MS
 #define RESET_TIME 10000
 
 static ETSTimer device_restart_timer;
@@ -29,14 +29,15 @@ void reset_call() {
 homekit_server_reset();
 wifi_config_reset();
 
-sdk_os_timer_setfn (& device_restart_timer, device_restart, NULL);
+```
+sdk_os_timer_setfn(& device_restart_timer, device_restart, NULL);
 sdk_os_timer_arm(&device_restart_timer, 5500, 0);
-
+```
 }
 
 void motion_sensor_callback(uint8_t gpio) {
 
-
+```
 if (gpio == MOTION_SENSOR_GPIO){
     int new = 0;
     new = gpio_read(MOTION_SENSOR_GPIO);
@@ -47,11 +48,11 @@ if (gpio == MOTION_SENSOR_GPIO){
 else {
     printf("Interrupt on %d", gpio);
 }
-
+```
 }
 
 void gpio_init() {
-gpio_enable(MOTION_SENSOR_GPIO, GPIO_OUTPUT);
+gpio_enable(MOTION_SENSOR_GPIO, GPIO_INPUT);
 gpio_set_pullup(MOTION_SENSOR_GPIO, true, true);
 gpio_set_interrupt(MOTION_SENSOR_GPIO, GPIO_INTTYPE_EDGE_ANY, motion_sensor_callback);
 }
@@ -66,20 +67,20 @@ xTaskCreate(identify_task, "identify", 128, NULL, 2, NULL);
 }
 
 homekit_characteristic_t name = HOMEKIT_CHARACTERISTIC_(NAME, "ESP8266");
-homekit_characteristic_t serial = HOMEKIT_CHARACTERISTIC_(SERIAL_NUMBER, "344000");
+homekit_characteristic_t serial = HOMEKIT_CHARACTERISTIC_(SERIAL_NUMBER, "Sonoff N/A");
 
 homekit_accessory_t *accessories[] = {
 HOMEKIT_ACCESSORY(
-  .id=10,
-  .category=homekit_accessory_category_sensor,
+  .id=1,
+  .category=homekit_accessory_category_switch,
         .services=(homekit_service_t*[]) {
     HOMEKIT_SERVICE(
         ACCESSORY_INFORMATION,
         .characteristics=(homekit_characteristic_t*[]) {
-            HOMEKIT_CHARACTERISTIC(NAME, "motion sensor"),
+            HOMEKIT_CHARACTERISTIC(NAME, "sensor"),
             HOMEKIT_CHARACTERISTIC(MANUFACTURER, "Alex_Khmelenko"),
-            HOMEKIT_CHARACTERISTIC(SERIAL_NUMBER, "344000"),
-            HOMEKIT_CHARACTERISTIC(MODEL, "motion_output"),
+            HOMEKIT_CHARACTERISTIC(SERIAL_NUMBER, "0012345"),
+            HOMEKIT_CHARACTERISTIC(MODEL, "motion"),
             HOMEKIT_CHARACTERISTIC(FIRMWARE_REVISION, "0.1"),
             HOMEKIT_CHARACTERISTIC(IDENTIFY, identify),
             NULL
@@ -96,20 +97,20 @@ NULL
 
 homekit_server_config_t config = {
 .accessories = accessories,
-.password = "111-11-111"
+.password = "123-45-678"
 };
 
 void create_accessory_name() {
 uint8_t macaddr[6];
 sdk_wifi_get_macaddr(STATION_IF, macaddr);
 
-
+```
 char *name_value = malloc(14);
 snprintf(name_value, 14, "Homekit-%02X%02X%02X", macaddr[3], macaddr[4], macaddr[5]);
 
 name.value = HOMEKIT_STRING(name_value);
 serial.value = name.value;
-
+```
 }
 
 void on_wifi_ready() {
@@ -119,9 +120,9 @@ create_accessory_name();
 void user_init(void) {
 uart_set_baud(0, 115200);
 
-
-wifi_config_init("MOTION_SENSOR", NULL, on_wifi_ready);
+```
+wifi_config_init("ESP", NULL, on_wifi_ready);
 homekit_server_init(&config);
 gpio_init();
-
+```
 }
